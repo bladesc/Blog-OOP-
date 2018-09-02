326fb73eb646e3c164f536f2c892ef3f72d222c1
bladesc <bladesc@interia.pl>
2018-09-02 23:39:42 +0200

    Added select method in DB class

--- public/Index.php
+++ public/Index.php
@@ -1,4 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 
-$connect = new Blog\Db;
+$db = new Blog\Db;
+$news = $db->selectData('*', 'news', ['id','=','1'],null,['id','ASC'] );
+
+echo "<pre>";
+print_r($news);
+echo "</pre>";
--- src/Db.php
+++ src/Db.php
@@ -5,6 +5,7 @@
  * Date: 02.09.2018
  * Time: 22:04
  */
+
 namespace Blog;
 
 class Db
@@ -19,10 +20,52 @@ class Db
     public function __construct()
     {
         try {
-            $this->dbConnection = new \PDO('mysql:host='.$this->dbHost.';dbname='.$this->dbName, $this->dbLogin, $this->dbPassword);
+            $this->dbConnection = new \PDO('mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName, $this->dbLogin, $this->dbPassword);
+            $this->dbConnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
-            return 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
+            return 'Connection failed: ' . $e->getMessage();
         }
     }
+
+    public function selectData(string $what = '*', string $from = '', array $where = [], $limit = false, array $orderBy = [])
+    {
+        $query = "SELECT $what";
+        $query .= " FROM $from";
+
+        if (!empty($where)) {
+            $query .= " WHERE $where[0] $where[1] $where[2]";
+        }
+        if ($limit) {
+            $query .= " LIMIT $limit";
+        }
+        if (!empty($orderBy)) {
+            $query .= " ORDER BY {$orderBy[0]} {$orderBy[1]}";
+        }
+        //echo $query; die;
+        $selectQuery = $this->dbConnection->prepare($query);
+        try {
+            $selectQuery->execute();
+        } catch (\PDOException $e) {
+            return 'Query failed: ' . $e->getMessage();
+        }
+
+        $result = $selectQuery->fetch();
+
+        if ($selectQuery->rowCount() > 0) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function closeConnection()
+    {
+    }
+
+    public function lastInsertId(): int
+    {
+        return $this->dbConnection->lastInsertId();
+    }
+
 
 }
