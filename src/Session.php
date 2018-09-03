01e7a1be2fbf3525d77f2ed33325f7a59549bc13
bladesc <bladesc@interia.pl>
2018-09-03 02:34:28 +0200

    added class

---
+++ src/Session.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panx
+ * Date: 03.09.2018
+ * Time: 02:01
+ */
---
+++ src/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panx
+ * Date: 03.09.2018
+ * Time: 02:01
+ */
+
+namespace Blog;
+
+
+class User
+{
+
+}
