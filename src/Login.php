015af24d06e330fae5c69eb471c4b42c72cedbc1
bladesc <bladesc@interia.pl>
2018-09-07 22:05:06 +0200

    finished login class

--- src/Login.php
+++ src/Login.php
@@ -10,48 +10,90 @@ namespace Blog;
 
 class Login
 {
+    /**
+     * Database object
+     *
+     * @var Db
+     */
     private $db;
+    /**
+     * User object
+     *
+     * @var
+     */
     private $user;
+    /**
+     * Session object
+     *
+     * @var Session
+     */
     private $session;
 
+    /**
+     * Array width messages
+     *
+     * @var array
+     */
     private $errorMessages = [];
 
+    /**
+     * Array with body of messages
+     *
+     * @var array
+     */
     private $textMessages = [
         "E-mail doesn't exist",
         "Incorrect password",
         "Login successful"
     ];
 
+    /**
+     * Login constructor.
+     *
+     * @param Session $session
+     * @param Db $db
+     */
     public function __construct(Session $session, Db $db)
     {
         if ($this->session == null) {
             $this->session = $session;
         }
-        
+
         if ($this->db == null) {
             $this->db = $db;
         }
-
     }
 
+    /**
+     * It login user
+     *
+     * @param User $user
+     */
     public function logIn(User $user): void
     {
         $this->user = $user;
 
-        $existUser = $this->userExist('users', $this->user->getEmail());
+        if ($this->userExist('users', $this->user->getEmail())) {
+            $existUser = $this->getUser('users', $this->user->getEmail());
 
-        if (!$existUser) {
-            $this->addMessage($this->textMessages[0]);
-        } else {
-            if ($this->verifyPassword($existUser['password'], $this->user->getPassword())) {
-                $this->addToSession($existUser);
-                $this->addMessage($this->textMessages[2]);
+            if (!$existUser) {
+                $this->addMessage($this->textMessages[0]);
             } else {
-                $this->addMessage($this->textMessages[1]);
+                if ($this->verifyPassword($existUser['password'], $this->user->getPassword())) {
+                    $this->addToSession($existUser);
+                    $this->addMessage($this->textMessages[2]);
+                } else {
+                    $this->addMessage($this->textMessages[1]);
+                }
             }
         }
     }
 
+    /**
+     * It adds user to session
+     *
+     * @param $existUser
+     */
     public function addToSession($existUser)
     {
         $userSession = [
@@ -63,29 +105,65 @@ class Login
         $this->session->setSession('loggedUser', $userSession);
     }
 
+    /**
+     * It verifies passed password width password from database
+     *
+     * @param string $databasePassword
+     * @param string $password
+     * @return bool
+     */
     public function verifyPassword(string $databasePassword, string $password): bool
     {
         return password_verify($password, $databasePassword) ? true : false;
     }
 
+    /**
+     * IT checks that user e-mail exist in database
+     *
+     * @param string $table
+     * @param string $email
+     * @return bool
+     */
     public function userExist(string $table, string $email)
     {
         $this->db->prepare("SELECT * FROM $table WHERE email = '$email'");
         $this->db->execute();
 
-        if ($this->db->getRowCount() === 0) {
-            return false;
-        } else {
-            return $existUser = $this->db->getRecord();
-        }
+        return $this->db->getRowCount() === 0 ? false : true;
     }
 
+    /**
+     * It return user data from database. User must exists
+     *
+     * @param string $table
+     * @param string $email
+     * @return array
+     */
+    public function getUser(string $table, string $email)
+    {
+        $this->db->prepare("SELECT * FROM $table WHERE email = '$email'");
+        $this->db->execute();
+
+        return $this->db->getRecord();
+    }
+
+    /**
+     * It's static method for logout user
+     *
+     * @param Session $session
+     */
     public static function logOut(Session $session): void
     {
         $session->deleteSession('userId');
         $session->destroySession();
     }
 
+    /**
+     * It's static method for check that user id logged in
+     *
+     * @param Session $session
+     * @return bool
+     */
     public static function isLogged(Session $session)
     {
         if ($session->issetSession('loggedUser')) {
@@ -95,11 +173,21 @@ class Login
         }
     }
 
+    /**
+     * It adds messages to array
+     *
+     * @param string $message
+     */
     public function addMessage(string $message): void
     {
         $this->errorMessages[] = $message;
     }
 
+    /**
+     * It shows messages
+     *
+     * @return array
+     */
     public function showMessage(): array
     {
         return $this->errorMessages;
