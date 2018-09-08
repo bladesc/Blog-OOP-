c664e562f45f5a0a800cce7cdd293e2f4b9355f0
bladesc <bladesc@interia.pl>
2018-09-08 23:52:40 +0200

    added view for comment

--- public/frontend/entry/entry-view.php
+++ public/frontend/entry/entry-view.php
@@ -9,20 +9,21 @@ use Blog\Redirect;
 use Blog\Comment;
 
 if (!isset($_GET['id'])) {
-  Redirect::redirectTo('/../404.php');
-  die("Id doesn't exist");
+    Redirect::redirectTo('/../404.php');
+    die("Id doesn't exist");
 }
-    //#ENTRIES#
-    $id = (int) $_GET['id'];
-    $db = new Db;
-    $entry = new Entry($db);
-    $entry = $entry->getById($id);
+//#ENTRIES#
+$id = (int)$_GET['id'];
+$db = new Db;
+$entry = new Entry($db);
+$entry = $entry->getById($id);
 
-    //#COMMENTS#
-    $db = new Db;
-    $comment = new Comment($db);
-    $comments = $comment->getByIdEntry($id);
+//#COMMENTS#
+$db = new Db;
+$comment = new Comment($db);
+$comments = $comment->getByIdEntry($id);
 ?>
+<?php include '../login-bar.php' ?>
 
 <div><?= $entry['id'] ?></div>
 <div><?= $entry['title'] ?></div>
@@ -30,8 +31,20 @@ if (!isset($_GET['id'])) {
 <div><?= Helper::changeDateFormat($entry['created_at']) ?></div>
 <div><?= Helper::changeDateFormat($entry['modified_at']) ?></div>
 
-<div>Komentarze <?= count($comments) ?></div>
+<div>Comments <?= count($comments) ?></div>
 <?php foreach ($comments as $comment): ?>
+    <div><?= $comment['login'] ?></div>
     <div><?= $comment['content'] ?></div>
 <?php endforeach; ?>
+
+<?php if ($loggedUser): ?>
+    <div>
+        <form method="post" action="">
+            <textarea name="content" placeholder="content..."></textarea>
+            <button type="submit" name="create">Add comment</button>
+        </form>
+    </div>
+<?php else: ?>
+    <div>Comments are accessible only for logged users</div>
+<?php endif; ?>
 
