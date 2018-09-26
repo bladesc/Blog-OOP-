022ddb85e13e1c7476e13fa6560e23f2ee2a96ae
Damian Szczesny <bladesc@interia.pl>
2018-09-26 23:50:41 +0200

    added new template file in backend

---
+++ public/backend/categories.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+?>
+
+<?php include 'template/layout-top-bar.php' ?>
+<?php include 'template/login-bar.php' ?>
+<?php include 'template/categories-bar.php' ?>
+<?php include 'template/bottom-bar.php' ?>
+
+
+
+
+
+
+
